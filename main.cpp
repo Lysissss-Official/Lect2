312db9b7c7a32fb0b52a9c94e0417f6619c2da4a
arch0git0 <al2_so4_3@163.com>
2026-05-23 11:23:31 +0800

    Build 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+//
+// Created by archeart on 2026/5/22.
+// ------------
+// main.cpp
+// 临时主程序替代
+// ------------
+//
+
 #include <iostream>
+#include "include/easyx/easyx.h"
+#include "include/easyx/graphics.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    initgraph(640, 480);
+    circle(200,200,100);
+    outtextxy(20, 20, "Hello, LectOS 2! 你好！");
+    getchar();
+    getchar();
     return 0;
 }
